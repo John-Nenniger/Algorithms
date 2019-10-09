8b58286b6ae2ec8e9b1973e1a0dab7f557f8d0a5
John Nenniger <JPNENNIGER@GMAIL.com>
2019-10-08 19:29:41 -0700

    work on decoding_letters

---
+++ decoding_letters.js
@@ -0,0 +1,26 @@
+// Good morning! Here's your coding interview problem for today.
+
+// This problem was asked by Facebook.
+
+// Given the mapping a = 1, b = 2, ...z = 26, and an encoded message, count the number of ways it can be decoded.
+
+// For example, the message '111' would give 3, since it could be decoded as 'aaa', 'ka', and 'ak'.
+
+// You can assume that the messages are decodable.For example, '001' is not allowed.
+
+
+function countPossibilities(letters){
+    // first transform letters into a number set
+    let possibilities = 0
+    let alpha = 'abcdefghijklmnopqrstuvwxyz'.split('')
+    let result_numbers = ''
+    letters.split('').forEach((letter) => {
+       result_numbers  = result_numbers + (alpha.indexOf(letter) + 1)
+    })
+
+    result_numbers.
+}
+
+
+
+countPossibilities('abc')
---
+++ lowest_positive_integer.rb
@@ -0,0 +1,11 @@
+
+
+def lowest_positive(array)
+    lowest = 0
+    array.each do |int|
+        if increment == lowest + 1
+            lowest  = lowest + 1
+        end
+    end
+
+end
